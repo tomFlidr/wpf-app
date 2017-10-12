f4f1a8321e717f96bc95754017bf31199279dafc
Tom Flídr <tomflidr@gmail.com>
2017-10-12 08:09:11 +0200

    base

---
+++ MainApp/Components/Accordion.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App.Components {
+	public enum AnimationStyle {
+		None,
+		Fast,
+		Slow
+	}
+	public partial class Accordion: Grid {
+
+		public AnimationStyle AnimationStyle {
+			get { return _animationStyle; }
+			set { _animationStyle = value; }
+		}
+		private AnimationStyle _animationStyle;
+
+		public int DefaultExpandedIndex {
+			get { return _defaultExpandedIndex; }
+			set { _defaultExpandedIndex = value; }
+		}
+		private int _defaultExpandedIndex = 0;
+
+		public Accordion () {
+			InitializeComponent();
+
+
+		}
+	}
+}
